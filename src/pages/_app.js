e8e50b4fb718a98fdabaf71f24dbc9e6535e99dd
Thiago Santos <t.mateus.santos2004@gmail.com>
2023-01-03 18:16:03 +0000

    sidebard

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
-
+import { SideBar } from '../components/SideBar'
 export default function App({ Component, pageProps }) {
+  <SideBar/>
   return <Component {...pageProps} />
 }
