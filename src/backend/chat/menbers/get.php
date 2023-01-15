830f2865c971547ad374bcdc3e71d558bbaa3cb3
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-01-15 03:20:40 +0000

    changes

---
+++ src/backend/chat/menbers/get.php
@@ -0,0 +1,99 @@
+<?php
+    /*
+    Get menber
+
+    -- Create tabe Menbers if not EXISTS
+    CREATE TABLE IF NOT EXISTS Menbers (
+        id INT NOT NULL AUTO_INCREMENT,
+        chatId INT NOT NULL,
+        role INT NOT NULL,
+        user INT NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (chatId) REFERENCES Chat(id),
+        FOREIGN KEY (role) REFERENCES Roles(id),
+        FOREIGN KEY (user) REFERENCES User(id)
+    ) ENGINE=INNODB;
+    
+    -- Create tabe User if not EXISTS
+    CREATE TABLE IF NOT EXISTS User (
+        id INT NOT NULL AUTO_INCREMENT,
+        Email VARCHAR(255) NOT NULL UNIQUE,
+        Password VARCHAR(255) NOT NULL,
+        Nome VARCHAR(255) NOT NULL,
+        Token VARCHAR(500) NOT NULL,
+        PRIMARY KEY (id)
+    ) ENGINE=INNODB;
+
+    -- Create tabe Roles if not EXISTS
+    CREATE TABLE IF NOT EXISTS Roles (
+        id INT NOT NULL AUTO_INCREMENT,
+        chatId INT NOT NULL,
+        role VARCHAR(255) NOT NULL,
+        PRIMARY KEY (id),
+        FOREIGN KEY (chatId) REFERENCES Chat(id)
+    ) ENGINE=INNODB;
+
+    For this use a innerjoin that return all fields from 
+    Menbers, User and Roles
+    Exept by User.Password
+
+    INNER JOIN:
+
+    SELECT Menbers.*, User.Email, User.Nome, User.Token, Roles.role
+    FROM Menbers
+    INNER JOIN User ON Menbers.user = User.id
+    INNER JOIN Roles ON Menbers.role = Roles.id;
+
+
+    url params:
+    chatId: int
+    token: string
+    */
+
+    // Include conn file
+    require_once "../../conn.php";
+
+    // Method get
+    if ($_SERVER["REQUEST_METHOD"] != "GET") {
+        http_response_code(405);
+        die();
+    }
+
+    // Get paramether from url and prevent null
+    $chatId = isset($_GET["chatId"]) ? $_GET["chatId"] : "";
+    $token = isset($_GET["token"]) ? $_GET["token"] : "";
+    // echo $chatId;
+    // echo $token;
+
+    // Create sql statement preventing the fields that can be empty
+    $sql = "";
+    $sql .= "SELECT Menbers.*, User.Email, User.Nome, User.Token, Roles.role ";
+    $sql .= "FROM Menbers ";
+    $sql .= "INNER JOIN User ON Menbers.user = User.id ";
+    $sql .= "INNER JOIN Roles ON Menbers.role = Roles.id ";
+
+    if(!empty($chatId) || !empty($token)) {
+        $sql .= "WHERE ";
+    }
+
+    if (!empty($chatId)) {
+        $sql .= "Menbers.chatId = $chatId";
+    }
+
+    if (!empty($token)) {
+        $sql .= "User.Token = '$token'";
+    }
+
+    $result = $conn->query($sql);
+    $rows = $result->fetch_all(MYSQLI_ASSOC);
+
+    // Return json whitout the password
+    echo json_encode(array(
+        "status" => "success",
+        "message" => "Menbers found",
+        "menbers" => $rows
+    ));
+
+
+    $conn->close();
+?>
--- src/backend/chat/menbers/setrole.php
+++ src/backend/chat/menbers/setrole.php
@@ -58,4 +58,113 @@
 
     // Get connetion
     include_once '../../conn.php';
+
+    // Verify if method is post
+    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+        // Return error
+        echo json_encode(array(
+            "status" => "error",
+            "message" => "Method not allowed"
+        ));
+        return;
+    }
+
+    // Get data
+    $data = json_decode(file_get_contents("php://input"));
+
+    // Verify if data is null
+    if ($data == null) {
+        // Return error
+        echo json_encode(array(
+            "status" => "error",
+            "message" => "Data is null"
+        ));
+        return;
+    }
+
+    // Get values from data
+    $token = $data->token;
+    $chat_idunique = $data->chat_idunique;
+    $user_id = $data->user_id;
+    $role = $data->role;
+
+    // Verify if values is null
+    if ($token == null || $chat_idunique == null || $user_id == null || $role == null) {
+        // Return error
+        echo json_encode(array(
+            "status" => "error",
+            "message" => "Given values is null"
+        ));
+        return;
+    }
+
+    // Validate Owner
+    /*
+    Using chat owner id get the user token
+    And compare with given token
+    */
+    $sql = "SELECT Token FROM User WHERE id = (SELECT owner FROM Chat WHERE IdUnique = '$chat_idunique')";
+    $result = mysqli_query($conn, $sql);
+    // Verify if token exists
+    if ($result->num_rows == 0) {
+        // Return error
+        echo json_encode(array(
+            "status" => "error",
+            "message" => "Token not found or invalid."
+        ));
+        return;
+    }
+
+    // Get chat id
+    $sql = "SELECT id FROM Chat WHERE IdUnique = '$chat_idunique'";
+    $result = mysqli_query($conn, $sql);
+    if ($result->num_rows == 0) {
+        // Return error
+        echo json_encode(array(
+            "status" => "error",
+            "message" => "Chat not found"
+        ));
+        return;
+    }
+    $chat_id = mysqli_fetch_assoc($result)['id'];
+
+    // Get role id
+    $sql = "SELECT id FROM Roles WHERE chatId = '$chat_id' AND role = '$role'";
+    $result = mysqli_query($conn, $sql);
+
+    // Verify if role exists
+    if ($result->num_rows == 0) {
+        // Return error
+        echo json_encode(array(
+            "status" => "error",
+            "message" => "Role not found"
+        ));
+        return;
+    }
+
+    // Get role id
+    $role_id = mysqli_fetch_assoc($result)['id'];
+
+    // Update role
+    $sql = "UPDATE Menbers SET role = '$role_id' WHERE chatId = '$chat_id' AND user = '$user_id'";
+    $result = mysqli_query($conn, $sql);
+
+    // Verify if role was updated
+    if (mysqli_affected_rows($conn) == 0) {
+        // Return error
+        echo json_encode(array(
+            "status" => "error",
+            "message" => "User role not updated"
+        ));
+        return;
+    }
+
+    // Return success
+    echo json_encode(array(
+        "status" => "success",
+        "message" => "User role updated"
+    ));
+
+    // Close connetion
+    mysqli_close($conn);
 ?>
