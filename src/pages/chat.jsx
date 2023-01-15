eabb460bf3f9674f3122b7a62276294a786db5c9
Thiago Santos <t.mateus.santos2004@gmail.com>
2023-01-15 21:54:47 +0000

    interface

---
+++ src/pages/chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const chat = () => {
+  return (
+<div className='min-h-full min-w-full flex items-center justify-center py-16 px-4 sm:px-6 lg:px-8 '>
+
+</div>
+  )
+}
+
+
+export default chat
