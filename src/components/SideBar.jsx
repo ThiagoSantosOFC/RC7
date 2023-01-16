141b06059fddeba95a28964b580ac0d4d8f2073d
Thiago Santos <t.mateus.santos2004@gmail.com>
2023-01-16 02:22:42 +0000

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -75,11 +75,11 @@ export const SideBar = () => {
   }
 
   return (
-    <div className="flex row  ">
+    <div className="flex flex-row min-h-full min-w-full  ">
       
   
 
-      <div className="rounded-r bg-gray-900 xl:hidden flex justify-between w-full p-6 items-center ">
+      <div className="rounded-r  bg-gray-900 xl:hidden flex justify-between w-full p-6 items-center ">
       
         <div className="flex justify-between  items-center space-x-3">
          
@@ -165,7 +165,7 @@ export const SideBar = () => {
       </div>
       <div
         id="Main"
-        className="xl:rounded-r transform  xl:translate-x-0  ease-in-out transition duration-500 flex justify-start items-start h-full  w-full sm:w-64 bg-gray-900 flex-col"
+        className="xl:rounded-r transform  xl:translate-x-0  ease-in-out transition duration-500 flex justify-start items-start min-h-full  min-w-75% sm:w-64 ring-4  bg-gray-900 flex-col"
       >
         <div className="hidden xl:flex justify-start p-6 items-center space-x-3">
           <Image
@@ -392,7 +392,7 @@ export const SideBar = () => {
           </button>
           <div
             id="menuQuarks"
-            className="flex flex-col justify-start items-start space-y-4 w-full"
+            className="flex flex-col justify-start items-start  w-75%"
           >
             <button className="flex justify-start items-center space-x-6 hover:text-white focus:bg-gray-700 focus:text-white hover:bg-gray-700 text-gray-400 rounded px-3 py-2  w-full md:w-52">
               <p className="text-sm leading-5  ">Criar Quark</p>
@@ -536,8 +536,9 @@ export const SideBar = () => {
    
         </div>
       </div>
-      <div className=" flex flex-col justify-center items-center w-full fixed top-10 border-spacing-2 ">
+      <div className=" flex flex-col justify-center items-center   w-full fixed  ">
        
+
         <p className="text-2xl leading-6 text-white">QuarkChat</p>
           
         </div>
