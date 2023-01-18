f59a9bbc36542fa00de6314c6d6b2da7f5e01fd0
Pedro Kaleb! <73436445+LyeZinho@users.noreply.github.com>
2023-01-18 09:14:15 +0000

    Update [link].js

--- src/pages/join/[link].js
+++ src/pages/join/[link].js
@@ -1,25 +1,70 @@
-import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import React from 'react'
+import Head from "next/head";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
-const Post = () => {
+const Join = () => {
   const router = useRouter();
-  const { link } = router.query;
+  //if user is not logged in, redirect to login page
+  const { link } = router.query
+ 
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      router.push("/login");
+    }
+    else{
+      /*
+      Do post request to:
+      http://localhost/backend/chat/invite/join.php
+      with body:
+      {
+        "link": "link",
+        "token": "token"
+      }
+      */
+      const token = localStorage.getItem("token");
+      const data = {
+        link: link ? link : "",
+        token: token ? token : ""
+      }
+      const dataJson = JSON.stringify(data);
 
-  /*
-  ENDPOINT: http://localhost/backend/chat/invite/join.php
-
-  JSON: 
-  {
-    "link": "link",
-    "token": "token"
-  }
-  */
+      try {
+        fetch("http://localhost/backend/chat/invite/join.php", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: dataJson,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.status === "success") {
+            router.push("/chat");
+          } else {
+            router.push("/error");
+          }
+        })
+      }
+      catch (err) {
+        console.log(err);
+      }
+    }
+  }, []);
+  
 
   return (
-    <div>
-      <h1>Joining {link}</h1>
-    </div>
+<div>
+      <Head>
+        <title>QuarkChat</title>
+        <meta name="description" content="Fale com seus amigos" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/assets/logo Quark.svg" />
+      </Head>
+</div>
   )
 }
 
-export default Post
+
+export default Join
